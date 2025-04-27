b2f5eb100e885fd7d1ee6d516dcd91d33575ce74
Adrien Jones <jones.ta@yahoo.com>
2025-04-27 14:09:50 -0500

    Created separate python file for Mazoyer rules and deleted rule list from test mazoyer py file

---
+++ mazoyer_rules.py
@@ -0,0 +1,28 @@
+class Mazoyer:
+    def l_rules(self):
+        l_transition_rules = {}
+        return l_transition_rules
+    
+    def a_rules(self):
+        a_transition_rules = {}
+        return a_transition_rules
+    
+    def b_rules(self):
+        b_transition_rules = {}
+        return b_transition_rules
+    
+    def c_rules(self):
+        c_transition_rules = {}
+        return c_transition_rules
+    
+    def g_rules(self):
+        g_transition_rules = {}
+        return g_transition_rules
+    
+    def h_rules(self):
+        h_transition_rules = {}
+        return h_transition_rules
+    
+    def r_rules(self):
+        r_transition_rules = {}
+        return r_transition_rules
